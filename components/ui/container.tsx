3148c1f40ff64019183d56e0a320a4b605c23b12
= <hornet.dev.0802@gmail.com>
2023-11-23 00:28:52 -0500

    # small fix

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,15 +1,9 @@
-interface ContainerProps{
-    children: React.ReactNode;
+interface ContainerProps {
+  children: React.ReactNode;
 }
 
-const Container:React.FC<ContainerProps> = ({
-    children
-}) => {
-    return(
-        <div className="mx-4 sm:mx-6 lg:mx-8 max-w-7xl">
-            {children}
-        </div>
-    )
-}
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div className="mx-4 sm:mx-6 lg:mx-8 max-w-7xl">{children}</div>;
+};
 
 export default Container;
