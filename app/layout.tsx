531e9dfe36e73c8e404bf0c78404fa0d08716753
Halsted312 <100381493+Halsted312@users.noreply.github.com>
2023-11-29 10:41:35 -0600

    took off the body part
    took out the body part seemed duplicated

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,14 +78,7 @@ export default function RootLayout({
 
 </head>
       
-      <body className={`${inter.className}`}>{children}
-      
-      		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8QXCC9J"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<!-- End Google Tag Manager (noscript) -->
-    
-    </body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
