79cd3d85e92ebf8c3fd838bbbb9e05bdf7527b38
Halsted312 <100381493+Halsted312@users.noreply.github.com>
2023-11-29 10:37:43 -0600

    updated headers on layout.txx
    added headers for google tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,68 @@ export default function RootLayout({
         currentStatus >= 0 && currentStatus <= 12 ? "black" : "black"
       }`}
     >
-      <body className={`${inter.className}`}>{children}</body>
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7051464837195925"
+     crossorigin="anonymous"></script>
+		 <!-- Google tag (gtag.js) -->
+		 <script async src="https://www.googletagmanager.com/gtag/js?id=G-G6SC9GX5RB"></script>
+		 <script>
+			  window.dataLayer = window.dataLayer || [];
+			  function gtag(){dataLayer.push(arguments);}
+			  gtag('js', new Date());
+			  gtag('config', 'G-G6SC9GX5RB');
+
+			  // Uncomment and use this line if you need to configure Google Ads
+			  gtag('config', 'AW-11415100394');
+		 </script>
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-3LTJ6P6R4Q"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'G-3LTJ6P6R4Q');
+	</script>
+
+
+	<!-- Event snippet for Page view conversion page -->
+	<script>
+		 gtag('event', 'conversion', {'send_to': 'AW-11415100394/EjTECMSQzfcYEOq3ksMq'});
+	</script>
+
+
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7051464837195925"
+		  crossorigin="anonymous"></script>
+
+	<!-- Twitter conversion tracking base code -->
+	<script>
+	!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+	},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+	a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+	twq('config','ohg64');
+	</script>
+	<!-- End Twitter conversion tracking base code -->
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-T8QXCC9J');</script>
+	<!-- End Google Tag Manager -->
+
+
+</head>
+      
+      <body className={`${inter.className}`}>{children}
+      
+      		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8QXCC9J"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+    
+    </body>
     </html>
   );
 }
